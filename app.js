4e5b403ff12e13fcd3cfe635858704407f05508f
I-A11 <d.islam1983@gmail.com>
2021-10-01 13:39:53 +1000

    adding add task, remove task and filter methods

--- app.js
+++ app.js
@@ -28,9 +28,44 @@ const addTask = (e) => {
 
   taskInput.value = "";
 };
+// Remove task
+const removeTask = (e) => {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    // console.log(e.target.parentElement);
+    if (confirm("Are You Sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+};
 
+// Clear Tasks
+const clearTasks = () => {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+};
+
+//filter tasks
+const filterTasks = (e) => {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll(".collection-item").forEach((task) => {
+    const item = task.firstChild.textContent;
+    if (item.toLocaleLowerCase().indexOf(text) !== -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
+
+  console.log(text);
+};
 const loadEventListeners = () => {
   form.addEventListener("submit", addTask);
+  taskList.addEventListener("click", removeTask);
+  // clear tasks
+  clearBtn.addEventListener("click", clearTasks);
+  // filter tasks
+  filter.addEventListener("keyup", filterTasks);
 };
 
 loadEventListeners();
